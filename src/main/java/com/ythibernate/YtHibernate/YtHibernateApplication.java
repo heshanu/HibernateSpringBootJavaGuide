7b565be12f519baa489a3cec31d7276da20ea054
heshanu <heshanu97@gmail.com>
2022-06-16 22:56:52 +0530

    Added

---
+++ src/main/java/com/ythibernate/YtHibernate/YtHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.ythibernate.YtHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtHibernateApplication.class, args);
+	}
+
+}
